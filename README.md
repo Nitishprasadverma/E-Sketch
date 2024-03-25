ab0fbebfb9d8821ae945df660ab9e986210a023d
Nitishprasadverma <nitishverma773964@gmail.com>
2024-03-25 15:20:07 +0530

    README file content is added to the readme file and comment in script file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # E-Sketch
+
+#[live Here](https://nitishprasadverma.github.io/E-Sketch/)
+The Etch-A-Sketch project is a web-based drawing tool inspired by the classic toy. Users can create colorful drawings by hovering over the grid squares.
+# Features
+* Grid Creation: Users can create a grid of customizable size to draw on.
+* Drawing Capability: Hovering over the grid squares applies color, allowing users to draw freely.
+* New Grid Button: Allows users to reset the grid to a new size and start drawing again.
+
+#  How to use
+1. Click the "New Grid" button to set the size of the drawing grid.
+2. Hover over the grid squares to draw.
+3. Click the "New Grid" button again to reset the grid and start a new drawing.
+
+# Try Live Demo Here:-
+
+Experience the Etch-A-Sketch tool in action [here](https://nitishprasadverma.github.io/E-Sketch/)
+
+
+# Implement BY:
+This Esketch-Pad is implemented by nitish.
+
+Wanna connect with me on [twitter](https://twitter.com/NitishV64066840)
+
+[Github](https://github.com/Nitishprasadverma)
+[Linkedin](https://www.linkedin.com/in/nitish-prasad-verma-094972253/);
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const body = document.body;
-
 const container = document.createElement("div");
 container.id = "container";
 body.appendChild(container);
@@ -7,7 +6,6 @@ body.appendChild(container);
 
 let grid = 16;
 let boxSize = 20;
-
 let containerWidth = (grid * boxSize);
 container.style.width = `${containerWidth}px`;
 createGrid();
@@ -23,54 +21,49 @@ function randomColor() {
 
 }
 
-// console.log(randomColor());
-
+// Function to create the grid boxes.
 function createGrid() {
     for (i = 0; i < grid * grid; i++) {
         const boxes = document.createElement("div");
         boxes.setAttribute("style", `height: ${boxSize}px; width: ${boxSize}px;`)
-
         boxes.classList.add("gridStyle", "square");
         container.appendChild(boxes)
     }
 }
 
 const hoverboxes = document.querySelectorAll(".square");
-
 hoverboxes.forEach(hoverboxes => {
     hoverboxes.addEventListener("mouseover", () => {
-        hoverboxes.style.backgroundColor = `${randomColor()}`;
+        hoverboxes.style.backgroundColor = `${randomColor()}`; //Adding hover event to add the background color to the grids boxes.
     });
 });
 
 const newGrid = document.querySelector("#newGrid");
-
+// event listener to the button new grid.
 newGrid.addEventListener("click", () => {
 
-
+    // removing the color of the all grids boxes.
     hoverboxes.forEach(hoverboxes => {
         hoverboxes.removeEventListener("mouseover", () => {
             hoverboxes.style.backgroundColor = `${randomColor()}`;
         });
-
     });
-
+    // to get the size of the grid from the user.
     let value = prompt("new grid size:");
     if (value > 100) {
         alert("please enter a value under 100 grids.");
         return;
     }
 
-    container.innerHTML = "";
+    container.innerHTML = ""; //once user entered value div container will change to empty.
     grid = value;
-
     let containerWidth = (grid * boxSize);
     container.style.width = `${containerWidth}px`;
+
+    //call the creategrid function again to create the grid boxes after clearing the container after value entered.
     createGrid();
-
-
-   let hoverboxe = document.querySelectorAll(".gridStyle");
-
+    // mouseover event listener to all the grid boxes to assigning the backgroundcolor agian.
+    let hoverboxe = document.querySelectorAll(".gridStyle");
     hoverboxe.forEach(hoverboxe => {
         hoverboxe.addEventListener("mouseover", () => {
             hoverboxe.style.backgroundColor = `${randomColor()}`;
